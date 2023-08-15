7ee95aaf5cabe038bd2ad8f8184e3ed5645270ed
Manish Agrahari <manish.agrahari2015@gmail.com>
2023-08-15 23:19:28 +0530

    commented the Model's file

--- models/Post.js
+++ models/Post.js
@@ -1,24 +1,33 @@
 const mongoose = require("mongoose");
 
+// Define the schema for the "Post" object
 const PostSchema = new mongoose.Schema(
   {
+    // userId field stores the ID of the user who created the post
     userId: {
       type: String,
-      required: true,
+      required: true, // The field is required, every post must have a user ID
     },
-    description: {
+    // desc field stores the description of the post, limited to 500 characters
+    desc: {
       type: String,
-      max: 500,
+      max: 500, // Maximum allowed length for the description
     },
-    image: {
+    // img field stores the URL or path of an image associated with the post
+    img: {
       type: String,
     },
+    // likes field stores an array of user IDs who have liked the post, default is an empty array
     likes: {
       type: Array,
-      default: [],
+      default: [], // The default value for the likes field is an empty array
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Adds createdAt and updatedAt fields to track creation and updates
 );
 
-module.exports = mongoose.model("Post", PostSchema);
+// Create a Mongoose model named "Post" using the defined schema
+const Post = mongoose.model("Post", PostSchema);
+
+// Export the "Post" model to be used in other parts of the application
+module.exports = Post;
--- models/User.js
+++ models/User.js
@@ -1,43 +1,55 @@
 const mongoose = require("mongoose");
 
+// Define the schema for the "User" object
 const userSchema = new mongoose.Schema(
   {
+    // username field stores the username of the user
     username: {
-      type: "string",
-      required: true,
-      min: 3,
-      max: 30,
-      unique: true,
+      type: String,
+      required: true, // The field is required, every user must have a username
+      min: 3, // Minimum length of the username
+      max: 30, // Maximum length of the username
+      unique: true, // Username must be unique
     },
+    // email field stores the email of the user
     email: {
-      type: "string",
-      required: true,
-      max: 30,
-      unique: true,
+      type: String,
+      required: true, // The field is required, every user must have an email
+      max: 30, // Maximum length of the email
+      unique: true, // Email must be unique
     },
+    // password field stores the password of the user
     password: {
-      type: "string",
-      required: true,
-      min: 6,
+      type: String,
+      required: true, // The field is required, every user must have a password
+      min: 6, // Minimum length of the password
     },
+    // desc field stores a description or bio of the user
     desc: {
-      type: "string",
-      min: 6,
+      type: String,
+      min: 6, // Minimum length of the description
     },
+    // followers field stores an array of user IDs who follow this user, default is an empty array
     followers: {
       type: Array,
-      default: [],
+      default: [], // Default value for the followers array is an empty array
     },
+    // following field stores an array of user IDs that this user follows, default is an empty array
     following: {
       type: Array,
-      default: [],
+      default: [], // Default value for the following array is an empty array
     },
+    // isAdmin field indicates whether the user is an admin, default is false
     isAdmin: {
       type: Boolean,
-      default: false,
+      default: false, // Default value for isAdmin is false
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Adds createdAt and updatedAt fields to track creation and updates
 );
 
-module.exports = mongoose.model("User", userSchema);
+// Create a Mongoose model named "User" using the defined schema
+const User = mongoose.model("User", userSchema);
+
+// Export the "User" model to be used in other parts of the application
+module.exports = User;
